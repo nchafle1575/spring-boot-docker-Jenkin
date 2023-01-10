e8a99ab5bdc4b8e24b0400de7ed2de1a49709726
nchafle1575 <nchafle@gmail.com>
2023-01-11 01:06:08 +0530

    remaining file

---
+++ src/main/java/com/example/springbootdockerJenkin/SpringBootDockerJenkinApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerJenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerJenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerJenkinApplication.class, args);
+	}
+
+}
